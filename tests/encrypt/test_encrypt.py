8c1f4e4841686bacc17919e78a279bfdf6a2032d
Stephani Rocha <gomes.stephani19@gmail.com>
2023-01-25 18:20:06 -0300

    feat(test_encrypt_message): Implementa teste para a função encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("trybe", -1) == "ebyrt"
+    assert encrypt_message("trybe", 3) == "yrt_eb"
+    assert encrypt_message("trybe", 4) == "e_byrt"
+    assert encrypt_message("trybe", 5) == "ebyrt"
+
+    with pytest.raises(TypeError):
+        encrypt_message(1, 2)
+
+    with pytest.raises(TypeError):
+        encrypt_message("trybe", "trybe")
