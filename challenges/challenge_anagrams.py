ac62332725380dc447f7c80a70f12ed2eea5daa5
Stephani Rocha <gomes.stephani19@gmail.com>
2023-01-26 18:25:46 -0300

    feat(is_anagram): Implementa a função is_anagram

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,20 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui!."""
-    raise NotImplementedError
+def is_anagram(first_string: str, second_string: str):
+    word_1 = list(first_string.lower())
+    string_1 = "".join(quicksort(word_1))
+    word_2 = list(second_string.lower())
+    string_2 = "".join(quicksort(word_2))
+
+    if string_1 == "" and string_2 == "":
+        return (string_1, string_2, False)
+    if string_1 == string_2:
+        return (string_1, string_2, True)
+    else:
+        return (string_1, string_2, False)
+
+
+def quicksort(lst):
+    if not lst:
+        return []
+    return (quicksort([x for x in lst[1:] if x < lst[0]])
+            + [lst[0]] +
+            quicksort([x for x in lst[1:] if x >= lst[0]]))
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,7 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    pass
+
+
+if __name__ == "__main__":
+    teste = is_palindrome_recursive("ab")
+    print(teste)
